2da1afff54733ea00b6337678859826b58b58388
Tirth Patel <tirthpatel330@gmail.com>
2022-04-14 02:21:29 +0000

    Identation changed in the main.tf of dev,prod,staging webserver.

--- terraform/webservers/prod-webserver/main.tf
+++ terraform/webservers/prod-webserver/main.tf
@@ -5,9 +5,9 @@ module "webservers-prod" {
   instance_type       = var.instance_type
   my_private_ip       = var.my_private_ip
   my_public_ip        = var.my_public_ip
-  desired_capacity = var.desired_capacity
-  max_size = var.max_size
-  min_size = var.min_size
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
   path_to_linux_key   = var.path_to_linux_key
   prefix              = module.globalvars.prefix
   default_tags        = module.globalvars.default_tags
